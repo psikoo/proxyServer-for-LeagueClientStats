2a663c40689f9a793c2239db75e04f358d3e1cbb
psikoo <hugohernandezmuelas03@gmail.com>
2024-05-17 22:05:52 +0200

    getIconAndLevel

--- proxyServer.js
+++ proxyServer.js
@@ -54,6 +54,25 @@ app.get("/getRiotID", async (req, res) => {
     res.json(RiotID)
 });
 
+app.get("/getIconAndLevel", async (req, res) => {
+    const queries = req.query;
+    if(!queries.server) {
+        res.json("no server provided")
+    } else if(!queries.PUUID) {
+        res.json("no PUUID provided")
+    }
+    const server = queries.server;
+    const PUUID = queries.PUUID;
+
+    const IconAndLevel = await axios.get(`https://${server}/lol/summoner/v4/summoners/by-puuid/${PUUID}${APIParam}`)
+    .then(res => {
+        return JSON.parse(`{"profileIconId": "${res.data.profileIconId}", "summonerLevel": "${res.data.summonerLevel}"}`);
+    }).catch((error) => {
+        console.log(`ERROR: ${error}`);
+    });
+    res.json(IconAndLevel)
+});
+
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
