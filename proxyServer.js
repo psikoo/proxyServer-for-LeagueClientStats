43cef7550f1e9ee53813ca373e1eac016af62090
psikoo <hugohernandezmuelas03@gmail.com>
2024-05-17 20:21:43 +0200

    Update proxyServer.js

--- proxyServer.js
+++ proxyServer.js
@@ -9,6 +9,9 @@ app.use(cors());
 const APIKey = "RGAPI-1643052f-39b4-42c6-bdd1-26bae937d3e9";
 const APIParam = `?api_key=${APIKey}`;
 
+app.get("/", function (req, res) {
+    res.send("working");
+})
 
 app.get("/getPUUID", async (req, res) => {
     const queries = req.query;
@@ -33,6 +36,6 @@ app.get("/getPUUID", async (req, res) => {
     res.json(PUUID)
 });
 
-app.listen(4000, function () {
-    console.log("Server started on port 4000");
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
 });
