845a7c217f5a63435c3cac95cd6b547ee193877e
psikoo <hugohernandezmuelas03@gmail.com>
2024-05-18 17:27:16 +0200

    added getXGameIDs and comments

--- proxyServer.js
+++ proxyServer.js
@@ -2,18 +2,23 @@ var express = require("express");
 var cors = require("cors");
 const axios = require("axios");
 
+// User cors mode with express
 var app = express();
-
 app.use(cors());
 
-const APIKey = "RGAPI-1643052f-39b4-42c6-bdd1-26bae937d3e9";
-const APIParam = `?api_key=${APIKey}`;
+// API key
+const APIKey = "RGAPI-9a6e955c-5237-4e8e-a36a-cfd478b0c384";
+const APIParam = `api_key=${APIKey}`;
 
+// Helps to see if the render.com server is up 
 app.get("/", function (req, res) {
-    res.send("working");
+    res.send("The server is up");
 })
 
+// Returns the PUUID of a user
+// Requires the "region", "gameName" and "tag" query parameters
 app.get("/getPUUID", async (req, res) => {
+    // Check if required query parameters are present
     const queries = req.query;
     if(!queries.region) {
         res.json("no region provided")
@@ -26,16 +31,20 @@ app.get("/getPUUID", async (req, res) => {
     const gameName = queries.gameName;
     const tag = queries.tag;
 
-    const PUUID = await axios.get(`https://${region}/riot/account/v1/accounts/by-riot-id/${gameName}/${tag}${APIParam}`)
+    // GET returns PUUID
+    const PUUID = await axios.get(`https://${region}/riot/account/v1/accounts/by-riot-id/${gameName}/${tag}?${APIParam}`)
     .then(res => {
         return res.data.puuid;
     }).catch((error) => {
-        console.log(`ERROR: ${error}`);
+        console.log(`/getPUUID ERROR: ${error}`);
     });
     res.json(PUUID)
 });
 
+// Returns the RiotID of a user
+// Requires the "region"and "PUUID" query parameters
 app.get("/getRiotID", async (req, res) => {
+    // Check if required query parameters are present
     const queries = req.query;
     if(!queries.region) {
         res.json("no region provided")
@@ -45,16 +54,20 @@ app.get("/getRiotID", async (req, res) => {
     const region = queries.region;
     const PUUID = queries.PUUID;
 
-    const RiotID = await axios.get(`https://${region}/riot/account/v1/accounts/by-puuid/${PUUID}${APIParam}`)
+    // GET returns a string with the RiotID (gameName#tag)
+    const RiotID = await axios.get(`https://${region}/riot/account/v1/accounts/by-puuid/${PUUID}?${APIParam}`)
     .then(res => {
         return `${res.data.gameName}#${res.data.tagLine}`;
     }).catch((error) => {
-        console.log(`ERROR: ${error}`);
+        console.log(`/getRiotID ERROR: ${error}`);
     });
     res.json(RiotID)
 });
 
+// Returns the icon and level of a user
+// Requires the "server"and "PUUID" query parameters
 app.get("/getIconAndLevel", async (req, res) => {
+    // Check if required query parameters are present
     const queries = req.query;
     if(!queries.server) {
         res.json("no server provided")
@@ -64,15 +77,49 @@ app.get("/getIconAndLevel", async (req, res) => {
     const server = queries.server;
     const PUUID = queries.PUUID;
 
-    const IconAndLevel = await axios.get(`https://${server}/lol/summoner/v4/summoners/by-puuid/${PUUID}${APIParam}`)
+    // GET returns a json object with the profileIconId and the summonerLevel
+    const IconAndLevel = await axios.get(`https://${server}/lol/summoner/v4/summoners/by-puuid/${PUUID}?${APIParam}`)
     .then(res => {
         return JSON.parse(`{"profileIconId": "${res.data.profileIconId}", "summonerLevel": "${res.data.summonerLevel}"}`);
     }).catch((error) => {
-        console.log(`ERROR: ${error}`);
+        console.log(`/getIconAndLevel ERROR: ${error}`);
     });
     res.json(IconAndLevel)
 });
 
+// Returns X amount of games from a user
+// Requires the "region", "PUUID" and "count" query parameters
+// Optional "startTime", "endTime", "type", "start" query parameters
+app.get("/getXGameIDs", async (req, res) => {
+    // Check if required query parameters are present
+    const queries = req.query;
+    if(!queries.region) {
+        res.json("no region provided");
+    } else if(!queries.PUUID) {
+        res.json("no PUUID provided");
+    } else if(!queries.count) {
+        res.json("no PUUID provided");
+    }
+    const region = queries.region;
+    const PUUID = queries.PUUID;
+    const count = queries.count;
+    // Optional query parameters
+    const startTime = queries.startTime;
+    const endTime = queries.endTime;
+    const type = queries.type;
+    const start = queries.start;
+
+    // GET returns a json object with X amount of games depending on the optional query parameters
+    const GameIDs = await axios.get(`https://${region}/lol/match/v5/matches/by-puuid/${PUUID}/ids?${APIParam}&count=${count}&start=${start}&startTime=${startTime}&endTime=${endTime}&type=${type}`)
+    .then(res => {
+        return `${res.data}`;
+    }).catch((error) => {
+        console.log(`/getXGameIDs ERROR: ${error}`);
+    });
+    res.json(GameIDs)
+});
+
+// Use port 3000 to connect to the server
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
