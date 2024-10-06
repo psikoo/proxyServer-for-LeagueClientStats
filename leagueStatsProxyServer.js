27822fc0b0d472ca7694297aa2710eff5cab86bb
psikoo <hugohernandezmuelas03@gmail.com>
2024-10-06 16:26:05 +0200

    added debug info

--- leagueStatsProxyServer.js
+++ leagueStatsProxyServer.js
@@ -37,7 +37,7 @@ app.get("/getPUUID", async (req, res) => {
     .then(res => {
         return res.data.puuid;
     }).catch((error) => {
-        console.log(`/getPUUID ERROR: ${error}`);
+        console.log(`/getPUUID ERROR: ${error.data}`);
     });
     res.json(PUUID)
 });
@@ -60,7 +60,7 @@ app.get("/getRiotID", async (req, res) => {
     .then(res => {
         return `${res.data.gameName}#${res.data.tagLine}`;
     }).catch((error) => {
-        console.log(`/getRiotID ERROR: ${error}`);
+        console.log(`/getRiotID ERROR: ${error.data}`);
     });
     res.json(RiotID)
 });
@@ -83,7 +83,7 @@ app.get("/getIconAndLevel", async (req, res) => {
     .then(res => {
         return JSON.parse(`{"profileIconId": "${res.data.profileIconId}", "summonerLevel": "${res.data.summonerLevel}"}`);
     }).catch((error) => {
-        console.log(`/getIconAndLevel ERROR: ${error}`);
+        console.log(`/getIconAndLevel ERROR: ${error.data}`);
     });
     res.json(IconAndLevel)
 });
@@ -115,7 +115,7 @@ app.get("/getXGameIDs", async (req, res) => {
     .then(res => {
         return `${res.data}`;
     }).catch((error) => {
-        console.log(`/getXGameIDs ERROR: ${error}`);
+        console.log(`/getXGameIDs ERROR: ${error.data}`);
     });
     res.json(GameIDs)
 });
